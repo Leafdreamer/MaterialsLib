54e83e189b546e2b873522f4f9df815e34797bab
Luna Nia <wamasterzofwiis@gmail.com>
2024-10-09 13:04:27 +0200

    and here we are

--- my-react-app/src/reducers/materialsReducer.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default function (state = [], action) {
-    switch (action.type) {
-        case 'FETCH_MATERIALS':
-            return action.payload;
-        default:
-            return state;
-    }
-}
